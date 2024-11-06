262ce75d09bb15b5054de9438358e78b8fe19f04
Magret <magretadekunle@gmail.com>
2024-11-06 17:09:04 -0600

    Dumping individual bouts (Cluster6) - better for model

--- Data-Transforming/data-cleaning-script.py
+++ Data-Transforming/data-cleaning-script.py
@@ -64,10 +64,7 @@ def clean_and_transform_data(config_file):
     })
 
     logging.info('Starting to dump individual bouts (Cluster6)')
-    # Step 1: Calculate the minimum length of lists in "Cluster6"
     min_length = transformed_df["Cluster6"].apply(len).min()
-
-    # Step 2: Filter out rows where the length of "Cluster6" is less than the min length
     df = transformed_df[transformed_df["Cluster6"].apply(len) >= min_length]
     logging.info(f'{(df.shape)} bouts were greater than or equal to {min_length} signals')
 
@@ -82,7 +79,7 @@ if __name__ == "__main__":
     # Transform     
     cleaned_data = clean_and_transform_data("./Data-Transforming/clean_data.json")
         
-    # Save final formatted data
+    # Save final formatted data (Clean, Transform Bouts to List, Drop all small Bout list )
     logging.info('Saving formatted data to CSV')
     cleaned_data.to_csv("./Data-Transforming/cleaned_data.csv", index=False)
 
